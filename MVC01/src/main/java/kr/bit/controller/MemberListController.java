276a58bce18496930055b7e418ef43f3c94a32a9
Jung-won-seok <ukksj0621@naver.com>
2024-04-23 13:11:04 +0900

    commit

--- MVC01/src/main/java/kr/bit/controller/MemberListController.java
+++ MVC01/src/main/java/kr/bit/controller/MemberListController.java
@@ -18,7 +18,7 @@ public class MemberListController extends HttpServlet {
 
 	protected void service(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
-		//1. 클라이언트의 요청을 받기(/memberList.do)
+		//1. 클라이언트의 요청 받기(/memberList.do)
 		//2. 회원전체 리스트 가져오기(Model 연동)
 		MemberDAO dao = new MemberDAO();
 		ArrayList<MemberVO> list = dao.memberList();
